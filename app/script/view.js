7fe9489c7e13cc4e2f59e7c639e0e40ed3a9c194
Anders Janmyr <anders@janmyr.com>
2014-01-10 14:49:15 +0100

    connected the view to the html

--- app/script/view.js
+++ app/script/view.js
@@ -3,11 +3,21 @@ window.view = function(model) {
 
     var Age = model.Age;
 
-    var age = new Age('1969-03-18');
-    console.log(age);
-    age.on('second', function() {
-        console.log('tick');
-    });
-    age.start();
+    var age;
+
+    var $age = $('#age');
+    var $birthDate = $('#birthdate');
+
+    $birthDate.on('change', function() {
+        age && age.stop();
+        age = new Age($birthDate.val());
+        console.log(age);
+        age.on('second', function() {
+            $age.text(age.born());
+        });
+        age.start();
+    })
+
+;
 };
 
